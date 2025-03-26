259e5cc1c59e47fda7fff4367bf3a3aead487773
ritesh7092 <riteshrajtiwari.iiitbh@gmail.com>
2025-03-26 23:24:12 +0530

    Find all Books , FindBook By id FindBook By Title Methods Added to BookService Class

--- src/main/java/com/Ajackus/library/repository/BookRepo.java
+++ src/main/java/com/Ajackus/library/repository/BookRepo.java
@@ -3,5 +3,8 @@ package com.Ajackus.library.repository;
 import com.Ajackus.library.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BookRepo extends JpaRepository<Book, String> {
+    Optional<Book> findByTitle(String title);
 }
--- src/main/java/com/Ajackus/library/service/BookService.java
+++ src/main/java/com/Ajackus/library/service/BookService.java
@@ -5,19 +5,28 @@ import com.Ajackus.library.repository.BookRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class BookService {
     @Autowired
     private BookRepo bookRepo;
 
     public Book addBook(Book book){
-        if (!"Available".equalsIgnoreCase(book.getAvailabilityStatus()) &&
-                !"Checked Out".equalsIgnoreCase(book.getAvailabilityStatus())) {
-            throw new IllegalArgumentException("Availability status must be 'Available' or 'Checked Out' ");
-        }
-        if (bookRepo.existsById(book.getBookId())) {
-            throw new IllegalArgumentException("Book ID must be unique");
-        }
         return bookRepo.save(book);
     }
+
+    public List<Book> findAllBooks() {
+        return bookRepo.findAll();
+    }
+
+    public Optional<Book> findBookById(String bookId) {
+        return bookRepo.findById(bookId);
+    }
+
+    public Optional<Book> findBookByTitle(String title) {
+        return bookRepo.findByTitle(title);
+    }
+
 }
