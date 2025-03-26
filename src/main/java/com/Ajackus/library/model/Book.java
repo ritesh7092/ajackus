85286f88b6c4dac4d8c92447f152315c60ccfca3
ritesh7092 <riteshrajtiwari.iiitbh@gmail.com>
2025-03-26 22:44:10 +0530

    Lombok Dependency Added and corresponding Changes for Getter and Setter and constructors in Book.java

--- src/main/java/com/Ajackus/library/model/Book.java
+++ src/main/java/com/Ajackus/library/model/Book.java
@@ -4,52 +4,18 @@ package com.Ajackus.library.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Book {
     @Id
     @NotBlank(message = "BookID can not be blank!")
     private String bookId;
-
-    public @NotBlank(message = "BookID can not be blank!") String getBookId() {
-        return bookId;
-    }
-
-    public void setBookId(@NotBlank(message = "BookID can not be blank!") String bookId) {
-        this.bookId = bookId;
-    }
-
-    public @NotBlank(message = "Title can not be blank!") String getTitle() {
-        return title;
-    }
-
-    public void setTitle(@NotBlank(message = "Title can not be blank!") String title) {
-        this.title = title;
-    }
-
-    public @NotBlank(message = "Author can not be blank!") String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(@NotBlank(message = "Author can not be blank!") String author) {
-        this.author = author;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public String getAvailabilityStatus() {
-        return availabilityStatus;
-    }
-
-    public void setAvailabilityStatus(String availabilityStatus) {
-        this.availabilityStatus = availabilityStatus;
-    }
 
     @NotBlank(message = "Title can not be blank!")
     private String title;
